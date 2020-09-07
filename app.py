e33783e244ed9322aa50a4d1a2805f74404df475
Emilio Bello <emiliobello@Emilios-MacBook-Pro.local>
2020-09-06 23:53:59 -0500

    Mars Fact table to HTML. Progress on the app

--- app.py
+++ app.py
@@ -1,22 +1,39 @@
 # importing dependencies
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_costa
+
 
 # Creating an instance of Flask
 app = Flask(__name__)
+
+# creating a connection variable
+conn = 'mongodb://localhost:27017'
+
+# passing the connectionn to the pymongo instance
+client = pymongo.MongoClient(conn)
+
+# connect to database
+db = client.mars_db
+
+# drop collection if available to remove duplicates
+# db.mars_mission.drop()
+
 
 # # Use PyMongo to establish Mongo connection
 # mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
 
 # Routing to render index.html template using data from Mongo
 @app.route("/")
+def index():
 
+
+    return render_template('index.html', )
 
 
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
+    
 
 
 
---
+++ scrape_mars.py
@@ -0,0 +1,7 @@
+# Import dependencies
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+
+# NASA Mars News
